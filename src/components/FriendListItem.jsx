d13a17f3b744a972a995a403c3b7b35a97941980
Piotr Wagner <wgr.animations@gmail.com>
2025-04-06 01:17:21 +0200

    Solved

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,15 +1,11 @@
-const FriendListItem = ({ id, avatar, name, isOnline }) => {
-  console.log({ id, avatar, name, isOnline });
-
-  return (
-    <li key={id} className="cardFriend">
-      <img className="imgFriend" src={avatar} alt="Avatar" width="70" />
-      <p className="name">{name}</p>
-      <p style={{ color: isOnline ? "green" : "red" }}>
-        {isOnline ? "Online" : "Offline"}
-      </p>
-    </li>
-  );
-};
+const FriendListItem = ({ avatar, name, isOnline }) => (
+  <div>
+    <img src={avatar} alt="Avatar" width="48" />
+    <p>{name}</p>
+    <p style={{ color: isOnline ? 'green' : 'red' }}>
+      {isOnline ? 'Online' : 'Offline'}
+    </p>
+  </div>
+);
 
 export default FriendListItem;

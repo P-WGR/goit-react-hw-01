b5794b24c60d050cbc564c556800675e0e4da9bd
Piotr Wagner <wgr.animations@gmail.com>
2025-04-06 01:44:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import Profile from './components/Profile.css';
-import FriendList from './components/FriendList.jsx';
-import TransactionHistory from './components/TransactionHistory.css';
-import userData from './components/userData.json';
-import friends from './components/friends.json';
-import transactions from './components/transactions.json';
-import './App.css';
+import "./App.css";
+import "./normalization.css";
+import "./components/Profile.css";
+import "./components/FriendList.css";
+import "./components/TransactionHistory.css";
+import Profile from "./components/Profile.jsx";
+import FriendList from "./components/FriendList.jsx";
+import TransactionHistory from "./components/TransactionHistory.jsx";
+import userData from "./components/userData.json";
+import friends from "./components/friends.json";
+import transactions from "./components/transactions.json";
 
 const App = () => {
   return (

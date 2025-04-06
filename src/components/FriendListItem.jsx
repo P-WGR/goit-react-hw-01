cae0c384684932a1136e5da3f7f5a0c8340c3308
Piotr Wagner <wgr.animations@gmail.com>
2025-04-06 02:05:02 +0200

    Update FriendListItem.jsx

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,13 +1,11 @@
-const FriendListItem = ({ avatar, name, isOnline }) => (
-  return (
-    <li key={id} className="cardFriend">
-      <img className="imgFriend" src={avatar} alt="Avatar" width="70" />
-      <p className="name">{name}</p>
-      <p style={{ color: isOnline ? "green" : "red" }}>
-        {isOnline ? "Online" : "Offline"}
-      </p>
-    </li>
-  );
-};
+const FriendListItem = ({ id, avatar, name, isOnline }) => (
+  <li key={id} className="cardFriend">
+    <img className="imgFriend" src={avatar} alt="Avatar" width="70" />
+    <p className="name">{name}</p>
+    <p style={{ color: isOnline ? "green" : "red" }}>
+      {isOnline ? "Online" : "Offline"}
+    </p>
+  </li>
+);
 
 export default FriendListItem;
